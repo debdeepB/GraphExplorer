1d81a2b2af5027a5ff97f6b1132e658cfcb196c5
Debdeep Basu <debdeepbasucal@hotmail.com>
2019-02-06 18:45:37 -0500

    Set up serializer for datasets and data

--- backend/datastore/models.py
+++ backend/datastore/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Dataset(models.Model):
+  name = models.CharField(max_length=200)
+
+class Data(models.Model):
+  file = models.FileField()
+  created_at = models.DateTimeField(auto_now=True)
+  dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+
+
--- backend/datastore/views.py
+++ backend/datastore/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DatasetSerializer
+from .models import Dataset
 
 # Create your views here.
+class DatasetView(viewsets.ModelViewSet):
+  serializer_class = DatasetSerializer
+  queryset = Dataset.objects.all()
