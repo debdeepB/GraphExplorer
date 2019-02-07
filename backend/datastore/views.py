77cd52849bab914f59afcfb89d948e16ab6977d3
Debdeep Basu <debdeepbasucal@hotmail.com>
2019-02-07 02:21:33 -0500

    Add dataset CRUD

--- backend/datastore/views.py
+++ backend/datastore/views.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .serializers import DatasetSerializer
 from .models import Dataset
+from rest_framework.response import Response
 
 # Create your views here.
-class DatasetView(viewsets.ModelViewSet):
-  serializer_class = DatasetSerializer
-  queryset = Dataset.objects.all()
+class DatasetView(viewsets.ViewSet):
+
+  def list(self, request):
+    queryset = Dataset.objects.all()
+    serializer = DatasetSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+  def retrieve(self, request, pk=None):
+    queryset = Dataset.objects.all()
+    dataset = get_object_or_404(queryset, pk=pk)
+    serializer = DatasetSerializer(dataset)
+    return Response(serializer.data)
+
+  def create(self, request):
+    dataset = Dataset.objects.create(name=request.data.get("name"))
+    dataset.save()
+    serializer = DatasetSerializer(dataset)
+    return Response(serializer.data)
+  
+
+
+
--- frontend/src/components/datasets/datasets.js
+++ frontend/src/components/datasets/datasets.js
@@ -1,20 +1,62 @@
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import axios from "axios";
 
 class Datasets extends Component {
-  state = {
-    data: []
-  };
-  componentDidMount() {
-    const url = "http://127.0.0.1:8000/api/datasets/";
+  constructor(props, context) {
+    super(props, context);
 
-    fetch(url)
-      .then(result => result.json())
-      .then(result => {
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      show: false,
+      data: [],
+      datasetName: ""
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleSubmit() {
+    this.setState({
+      show: false
+    });
+    axios
+      .post("http://127.0.0.1:8000/api/datasets/", {
+        name: this.state.datasetName
+      })
+      .then(res => {
+        var clonedArray = this.state.data.slice();
+        clonedArray.push(res.data);
         this.setState({
-          data: result
+          data: clonedArray
         });
       });
+  }
+
+  handleChange(event) {
+    this.setState({ datasetName: event.target.value });
+  }
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/api/datasets/").then(res => {
+      this.setState({
+        data: res.data
+      });
+    });
   }
   render() {
     const { data } = this.state;
@@ -29,6 +71,34 @@ class Datasets extends Component {
     });
     return (
       <div>
+        <Button variant="primary" onClick={this.handleShow}>
+          Add Dataset
+        </Button>
+        <br />
+        <br />
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Dataset</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <FormControl
+                type="text"
+                placeholder="Add your dataset"
+                value={this.state.datasetName}
+                onChange={this.handleChange}
+              />
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleSubmit}>
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <Table striped bordered hover>
           <thead>
             <tr>
