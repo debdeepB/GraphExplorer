d13f7bb375cd6f14482f058c1f5516355a698c6d
Debdeep Basu <debdeepbasucal@hotmail.com>
2019-02-15 14:20:25 -0500

    Add visas graph and update navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,9 @@ library.add(faTrash);
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Datasets from "./components/datasets/datasets";
 import Dataset from "./components/datasets/dataset";
+import Evaluation from "./components/evaluations/evaluation";
 import NavBar from "./components/navbar";
-
+import "./App.css";
 class App extends Component {
   render() {
     return (
@@ -22,6 +23,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/datasets" component={Datasets} />
             <Route exact path="/datasets/:datasetId" component={Dataset} />
+            <Route exact path="/evaluate/:datasetId" component={Evaluation} />
           </Switch>
         </div>
       </Router>
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -4,12 +4,12 @@ import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 class NavBar extends Component {
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
+      <Navbar className="fixed-top" bg="dark" variant="dark">
         <Navbar.Brand href="/">AIDA Evaluation Framework</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/datasets/">EDA</Nav.Link>
           <Nav.Link to="/">Train</Nav.Link>
-          <Nav.Link to="/">Evaluate</Nav.Link>
+          <Nav.Link href="/evaluate/66">Evaluate</Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
